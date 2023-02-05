3d652fe1025121430fe0c16c98c30f54ddea3282
pythone-developer <saraheeeen@gmail.com>
2023-02-05 14:35:08 +0200

    TaskCreat

--- Base/views.py
+++ Base/views.py
@@ -14,3 +14,8 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model=Task
     context_object_name='task'
+
+class TaskCreat(CreateView):
+    model=Task
+    fields='__all__'
+    #success_url=redirect('tasks')
